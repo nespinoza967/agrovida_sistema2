9583d9ce6bd9edb431959220414253b48151b30a
Nicolas Espinoza <nalejandro967@gmail.com>
2026-09-11 18:03:32 -0300

    commit 3

---
+++ catalogo/data.py
@@ -0,0 +1,5 @@
+productos = [
+    {"nombre": "Fertilizante NPK 15-15-15", "categoria": "Fertilizantes", "precio": 18990, "stock": 120},
+    {"nombre": "Semilla de Maíz Híbrido", "categoria": "Semillas", "precio": 5990, "stock": 340},
+    {"nombre": "Herbicida Glifosato 20L", "categoria": "Agroquímicos", "precio": 45990, "stock": 25},
+]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
+from . import data
 
-# Create your views here.
+def productos(request):
+    return render(request, 'index.html', {'productos': data.productos})
+
+def producto_detail(request, nombre):
+    producto = next((p for p in data.productos if p['nombre'] == nombre), None)
+    if producto is None:
+        raise Http404("Producto no encontrado")
+    return render(request, 'index.html', {'producto': producto})
